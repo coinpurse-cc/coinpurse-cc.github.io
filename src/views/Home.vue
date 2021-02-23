8937eb27370e785f11f78b0710bde490c672c297
Andy <admazzola@gmail.com>
2021-02-23 18:26:20 -0500

    front page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -20,13 +20,50 @@
       <section class="hero   "  >
         <div class="hero-body">
 
+            <div class="whitespace-sm"></div>
+
+               <div class="flex flex-col content-center justify-center w-full">  
+                 <div class="w-full">  Free, feeless, non-custodial cryptocurrency bot for Discord  </div>
+                   
+                <div class="w-full text-gray-500 text-xs"   >  Powered by xDai Network + Ethereum + PoW Mineable Tokens</div>
+               </div>
+
+
+
+            <div class="whitespace-sm"></div>
+
+         <div class="flex  ">  
+              <img style="margin:0 auto" src="/assets/images/discordcollage.jpg"></img>
+        </div>
+
+          <div class="whitespace-md"></div>
+            <div class="flex flex-col content-center justify-center w-full ">  
+              <div  style="margin:0 auto" >
+               <a  class=" mx-2 p-4 bg-purple-600 hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-opacity-50" href="https://discord.com/api/oauth2/authorize?client_id=813641559265378344&permissions=3072&scope=bot" target="_blank">
+                Invite Coinpurse Bot into Discord! </a> 
+
+
+                 <a   class="mx-2  p-4 bg-orange-600 hover:bg-orange-700 focus:outline-none focus:ring-2 focus:ring-orange-600 focus:ring-opacity-50" href="https://discord.com/invite/JGEqqmS" target="_blank">
+                Try it out in the 0xBTC Discord </a> 
+            </div>
+                
+
+
+            </div> 
+
             <div class="whitespace-md"></div>
 
+            <div> 
+              <div class="text-lg text-white"> Getting Started </div>
 
-               Coinpurse
+              <div class="text-md  "> -Once the bot is invited to your server, type '!wallet' in the chat for commands. </div>
+              <div class="text-md  "> -Users will need to install <a class="text-blue-500" href="https://metamask.io">Metamask</a> and <a class="text-blue-500" href="https://www.xdaichain.com/for-users/wallets/metamask/metamask-setup">Configure it for xDai Network</a> to send tips. </div>
 
 
-                
+               <div class="text-md  "> That's it! </div>
+
+
+            </div>  
 
  
         </div>
